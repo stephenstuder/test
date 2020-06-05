40413cdb970d9ffed44e9d0a87df08281f863150
Stephen Studer <stephenkstuder@gmail.com>
2020-06-05 07:46:11 -0700

    Vue instance is the heart of the application, it ties to an element and alls for passing of variables that can be updated

---
+++ main.js
@@ -0,0 +1,7 @@
+let app = new Vue ({
+    el: '#app',
+    data: {
+        product: 'Boots',
+        description: 'These boots are made for walking, and that\'s just what they do. One of these days these boots are gonna walk all over you.'
+    }
+})
