668bd656adee9b2d19d06d224cc1131485ef886d
Stephen Studer <stephenkstuder@gmail.com>
2020-06-05 07:57:31 -0700

    v-bind: dynamically binds an attribute to an expression, short hand of : is allowed

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let app = new Vue ({
     el: '#app',
     data: {
-        product: 'Boots',
-        description: 'These boots are made for walking, and that\'s just what they do. One of these days these boots are gonna walk all over you.'
+        product: 'Socks',
+        image: './assets/vm-socks.png',
+        url: 'https://amazon.com'    
     }
 })
