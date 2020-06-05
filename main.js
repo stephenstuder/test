b36cb2b546194ad1f68b5b97e23023be1dc5a797
Stephen Studer <stephenkstuder@gmail.com>
2020-06-05 12:09:02 -0700

    handle events with the v-on attribute, declare them under your data inside of a methods object

--- main.js
+++ main.js
@@ -2,19 +2,33 @@ let app = new Vue ({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/vm-socks.png',
+        image: './assets/vmSocks-green.png',
         inventory: 100,
         inStock: true,
         details: ["80% cotton", "20% polyester", "Gender neutral"],   
         variants: [
             {
                 variantId: 2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green.png"
             },
             {
                 variantId: 2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue.jpg"
             }
-        ]
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage){
+            this.image = variantImage;
+        },
+        emptyCart(){
+            this.cart = 0;
+        }
     }
 })
