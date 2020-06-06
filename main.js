d49cf19ce974ea3c2c19ace9a7d90f677d69b0d5
Stephen Studer <stephenkstuder@gmail.com>
2020-06-06 13:42:29 -0700

    Components are blocks of code, grouped together within a custom element, data on a component must be a function

--- main.js
+++ main.js
@@ -1,34 +1,121 @@
-let app = new Vue ({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        image: './assets/vmSocks-green.png',
-        inventory: 100,
-        inStock: false,
-        details: ["80% cotton", "20% polyester", "Gender neutral"],   
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: "green",
-                variantImage: "./assets/vmSocks-green.png"
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue",
-                variantImage: "./assets/vmSocks-blue.jpg"
-            }
-        ],
-        cart: 0
+
+// Create a component called product-details with a prop called details
+Vue.component("product-details", {
+  props: {
+    details: {
+      type: Array,
+      required: true,
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
+  },
+  template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        `,
+});
+
+Vue.component("product", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template: `     
+  <div class="product">
+    <div class="product-image">
+        <img :src="image" alt="" />
+    </div>
+    <div class="product-info">
+        <h1>{{title}}</h1>
+        <div class="details">
+            <p class="onSale" v-if="onSale">On Sale!</p>
+            <p v-if="inStock">In Stock</p>
+            <p v-else 
+                :class="{outOfStock: !inStock}">Out of Stock</p>
+                </div>
+                <p>Shipping: {{ shipping }}</p>
+
+
+                <product-details :details="details"></product-details>
+        <ul>
+            <div v-for="(variant, index) in variants" 
+            :key="variant.variantId"
+            class="color-box"
+            :style="{ backgroundColor: variant.variantColor }"
+            @mouseover="updateProduct(index)">
+            </div>
+        </ul>
+        
+        <button v-on:click="addToCart" 
+                :disabled="!inStock"
+                :class="{disabledButton: !inStock}">Add to Cart</button>
+        <div class="cart">
+            <p>Cart({{cart}})</p>
+        </div>
+        <button v-on:click="emptyCart">Empty Cart</button>
+    </div>
+    </div>`,
+  data() {
+    return {
+      brand: "Vue Mastery",
+      product: "Socks",
+      selectedVariant: 0,
+      details: ["80% cotton", "20% polyester", "Gender neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage: "./assets/vmSocks-green.jpg",
+          variantQuantity: 10,
+          variantOnSale: true,
         },
-        updateProduct(variantImage){
-            this.image = variantImage;
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage: "./assets/vmSocks-blue.jpg",
+          VariantQuantity: 0,
+          variantOnSale: false,
         },
-        emptyCart(){
-            this.cart = 0;
-        }
-    }
-})
+      ],
+      cart: 0,
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(index) {
+      this.selectedVariant = index;    },
+    emptyCart() {
+      this.cart = 0;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    onSale() {
+      return this.variants[this.selectedVariant].variantOnSale;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      }
+      return 2.99;
+    },
+  },
+});
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    premium: false,
+  },
+});
